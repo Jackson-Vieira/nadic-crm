f33848871f280d0708cf83720f101380c2cd9b9e
Jackson-Vieira <jacksonvieiira12@gmail.com>
2022-11-22 23:03:29 -0300

    setup templates

--- estoque/admin.py
+++ estoque/admin.py
@@ -1,2 +1,9 @@
 from django.contrib import admin
 
+from .models import Company, Product, Order, Inventory, Sale
+
+admin.site.register(Company)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(Inventory)
+admin.site.register(Sale)
--- estoque/views.py
+++ estoque/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+# class RegistryInventory(View):
+#     form_class = ''
+#     initial = {'key': 'value'}
+#     template_name = 'form_template.html'
+
+#     def get(self, request, *args, **kwargs):
+#         form = self.form_class(initial=self.initial)
+#         return render(request, self.template_name, {'form': form})
+
+#     def post(self, request, *args, **kwargs):
+#         form = self.form_class(request.POST)
+#         if form.is_valid():
+#             # <process form cleaned data>
+#             return HttpResponseRedirect('/success/')
+#         return render(request, self.template_name, {'form': form})
+

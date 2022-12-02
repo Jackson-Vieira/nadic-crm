c20a55c4b3f1f3e360f13f71480ce5c0d20b1d62
Jackson-Vieira <jacksonvieiira12@gmail.com>
2022-12-02 20:34:10 -0300

    viewsets updates

--- empresa/api/v2/viewsets.py
+++ empresa/api/v2/viewsets.py
@@ -12,15 +12,13 @@ from rest_framework.mixins import (
     )
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-
 from django_filters import rest_framework as filters
 
 from ...models import Company, Product, Inventory, Registry
-from ..serializers import CompanySerializer, ProductSerializer, RegistrySerializer
+from ..serializers import CompanySerializer, ProductSerializer, RegistrySerializer, InventorySerializer
 from .filters import *
 from ..utils.validators import user_has_company
 
-# V2 API
 class CompanyViewSet(viewsets.ModelViewSet):
     model = Company
     queryset = Company.objects.all()
@@ -32,28 +30,61 @@ class CompanyViewSet(viewsets.ModelViewSet):
         if not user_has_company(request.user):
             serializer.is_valid(raise_exception=True)
             serializer.save(owner=request.user)
-            headers = self.get_success_headers(serializer.data)
-            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response({'message': 'there is already a company linked to this user'}) # change status
 
-    # stats -> action - apenas o dono da empresa
-    # retrieve
-    # products
-    # registrys
+    @action(detail=True, methods=['GET'])
+    def stats(self, request, pk = None):
+        company = self.get_object()
+        return Response(
+            {
+            'stats': {
+                'total_billing': company.total_billing,
+                }
+            }
+        )
 
 class ProductViewSet(viewsets.ModelViewSet):
     model = Product
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, ] # custom permission
 
     def create(self, request, *args, **kwargs):
-        pass
-    # action inventory - GET, PUT
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(company=request.user.company)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+    @action(detail=True, methods=['GET'], serializer_class=InventorySerializer)
+    def inventory(self, request, pk = None):
+        product = self.get_object()
+        inventory = product.inventory
+        serializer = self.get_serializer(instance=inventory)
+        return Response(serializer.data)
+        
+    @inventory.mapping.put
+    def update_inventory(self, request, pk = None):
+        product = self.get_object()
+        inventory = product.inventory
+        serializer = InventorySerializer(instance=inventory, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
 
 class RegistryViewSet(viewsets.ModelViewSet):
-     # only post and get methods
+    # only post and get methods
     model = Registry
     queryset = Registry.objects.all()
     serializer_class = RegistrySerializer 
 
-   
+    # ListModelMixin, 
+    # CreateModelMixin
+    # RetrieveModelMixin -  Only visible for the owner of the empresa
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(company=request.user.company)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)

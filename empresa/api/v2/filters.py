09a7daab0e0a2dc837f791cd364a8514992c2e43
Jackson-Vieira <jacksonvieiira12@gmail.com>
2022-12-03 13:18:16 -0300

    fix filter field company error

--- empresa/api/v2/filters.py
+++ empresa/api/v2/filters.py
@@ -3,13 +3,11 @@ from django_filters import rest_framework as filters
 from ...models import Product, Registry
 
 class ProductFilterSet(filters.FilterSet):
-
-
     keyword = filters.CharFilter(field_name='name', lookup_expr='icontains')
-    
+    company = filters.CharFilter(field_name='company__name', lookup_expr='exact')
+
     # min_inventory_quantity 
     # max_inventory_quantity
-
     min_price = filters.NumberFilter(field_name='price', lookup_expr='gte')
     max_price = filters.NumberFilter(field_name='price', lookup_expr='lte')
 
@@ -20,12 +18,12 @@ class ProductFilterSet(filters.FilterSet):
 
 class RegistrysFilterSet(filters.FilterSet):
 
+    company = filters.CharFilter(field_name='company__name', lookup_expr='exact')
+
     min_product_quantity =  filters.NumberFilter(field_name='product_quantity', lookup_expr='gte')
     max_product_quantity = filters.NumberFilter(field_name='product_quantity', lookup_expr='lte')
-
     created_start = filters.DateFilter(field_name='created_at', lookup_expr='gte')
     created_end = filters.DateFilter(field_name='created', lookup_expr='lte')
-
 
     class Meta:
         model = Registry
